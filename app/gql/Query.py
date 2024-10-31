e3120d42cbc1abbd56875f50ef7579b49ca0ed53
nobody <yzeviw@gmail.com>
2024-10-31 12:20:17 +0200

    added the graphql queries and set app the flask server

---
+++ app/gql/Query.py
@@ -0,0 +1,36 @@
+from app.gql.types import MissionType
+from graphene import ObjectType, List, String, Field, Int
+import app.database.repository.mission_repository as mission_repos
+from sqlalchemy import Date
+
+
+class Query(ObjectType):
+    mission_by_id = Field(MissionType, mission_id=Int())
+    missions_by_date = List(MissionType, mission_date=String())
+    missions_by_country = List(MissionType, country=String())
+    missions_by_target_industry = List(MissionType, industry=String())
+    mission_results_by_target_type = List(MissionType, target_type=String())
+
+
+    @staticmethod
+    def resolve_mission_by_id(root, info, mission_id):
+        return (mission_repos.get_mission_by_id(mission_id)
+                .value_or(f"couldnt find mission by id {mission_id}"))
+
+
+    @staticmethod
+    def resolve_missions_by_date(root, info, mission_date):
+        return mission_repos.get_mission_by_date(mission_date)
+
+
+    @staticmethod
+    def resolve_missions_by_country(root, info, country):
+        return mission_repos.get_mission_by_country(country)
+
+    @staticmethod
+    def resolve_missions_by_target_industry(root, info, industry):
+        return mission_repos.get_mission_by_target_industry(industry)
+
+    @staticmethod
+    def resolve_mission_results_by_target_type(root, info, target_type):
+        return mission_repos.get_mission_results_by_target_type(target_type)
--- app/main.py
+++ app/main.py
@@ -1,10 +1,20 @@
-from app.database.model import *
-from app.database.connection.psql import get_session
+from app.gql.Query import Query
+from flask import Flask
+from graphene import Schema
+from flask_graphql import GraphQLView
 
 
+schema = Schema(query=Query)
+app = Flask(__name__)
+app.add_url_rule(
+    '/graphql',
+    view_func=GraphQLView.as_view(
+        "graphql",
+        schema=schema,
+        graphiql=True
+    )
+)
 
 
 if __name__ == "__main__":
-    with get_session() as session:
-        a = session.query(Mission).all()
-        print(a)
+    app.run(host='0.0.0.0', port=5000)
